66134cce1786f319fb8796a1c746941fdc91b984
Pranav <kumarpranavv.097@gmail.com>
2026-07-03 19:43:22 +0530

    using "if" statements to determine if x is greater than y.

---
+++ lab/CompareNumbers.java
@@ -0,0 +1,15 @@
+package lab;
+
+class CompareNumbers {
+    public static void main(String[] args) {
+
+        int x = 25;
+        int y = 15;
+
+        if (x > y) {
+            System.out.println("X is greater than Y.");
+        } else {
+            System.out.println("X is not greater than Y.");
+        }
+    }
+}
